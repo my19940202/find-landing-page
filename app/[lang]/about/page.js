7f7d8c441103db0125efe8055ff26cb37eab88e9
xishengbo <xishengbo@baidu.com>
2024-07-05 22:31:05 +0800

    first commit

---
+++ app/[lang]/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+	return <>
+        <div
+            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+        />
+    </>;
+}
---
+++ app/[lang]/blog/page.js
@@ -0,0 +1,3 @@
+export default function About() {
+	return <>this is blog blogblogblog</>;
+}
