e9911e2d865c632a501fb3e6c5adb06cc986ba74
xishengbo <xishengbo@baidu.com>
2024-08-25 17:06:15 +0800

    remove useless code &&  add preview img

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -1,14 +1,14 @@
-'use client';
-import Image from 'next/image';
-import { MdMenu } from 'react-icons/md';
-import { SiGithub } from 'react-icons/si';
-import { useEffect, useState } from 'react';
-import ThemeToggle from './themeToggle';
-import LangSwitch from './langSwitch';
+"use client";
+import Image from "next/image";
+import { MdMenu } from "react-icons/md";
+import { SiGithub } from "react-icons/si";
+import { useEffect, useState } from "react";
+import ThemeToggle from "./themeToggle";
+import LangSwitch from "./langSwitch";
 
-import { usePathname } from 'next/navigation';
-import { defaultLocale } from '@/lib/i18n';
-import { NavLinksList } from '@/lib/navLinksList';
+import { usePathname } from "next/navigation";
+import { defaultLocale } from "@/lib/i18n";
+import { NavLinksList } from "@/lib/navLinksList";
 
 export default function Navbar() {
     const pathname = usePathname();
@@ -17,10 +17,10 @@ export default function Navbar() {
 
     useEffect(() => {
         const fetchLinksList = async () => {
-            if (pathname === '/') {
+            if (pathname === "/") {
                 setLangName(defaultLocale);
             } else {
-                setLangName(pathname.split('/')[1]);
+                setLangName(pathname.split("/")[1]);
             }
             setLinkList(NavLinksList[`LINK_${langName.toUpperCase()}`] || []);
         };
@@ -28,52 +28,49 @@ export default function Navbar() {
     }, [pathname, langName]);
 
     return (
-        <header className='w-full z-50 bg-base-100 p-5 pb-0 max-w-[1280px] mx-auto md:mb-5 flex justify-between items-center'>
+        <header className="w-full z-50 bg-base-100 p-5 pb-0 max-w-[1280px] mx-auto md:mb-5 flex justify-between items-center">
             <a
-                aria-label='keyword finder'
-                className='flex items-center w-1/2 md:w-1/5'
-                title='keyword finder'
+                aria-label="keyword finder"
+                className="flex items-center w-1/2 md:w-1/5"
+                title="keyword finder"
                 href={`/${langName}`}
             >
                 <Image
                     width={200}
                     height={200}
-                    src={'/logo.gif'}
-                    className='transition-all hover:scale-110 w-6 md:w-10 h-6 md:h-10'
-                    alt='logo'
+                    src={"/icon.png"}
+                    className="transition-all hover:scale-110 w-6 md:w-10 h-6 md:h-10"
+                    alt="logo"
                 ></Image>
-                <h2 className='ml-3 font-bold leading-5'>Keyword Finder</h2>
+                <h2 className="ml-3 font-bold leading-5">Keyword Finder</h2>
             </a>
 
-            <ul className='w-3/5 px-5 font-medium hidden md:flex flex-nowrap items-center justify-around'>
+            <ul className="w-3/5 px-5 font-medium hidden md:flex flex-nowrap items-center justify-around">
                 {linkList.map((link, index) => {
                     return (
-                        <li
-                            key={index}
-                            className='group py-3 text-center'
-                        >
+                        <li key={index} className="group py-3 text-center">
                             <a
                                 aria-label={link.name}
-                                className='group relative'
+                                className="group relative"
                                 title={link.name}
                                 href={`/${langName}${link.url}`}
                             >
                                 {link.name}
-                                <div className='absolute left-[50%] group-hover:left-0 w-0 group-hover:w-full h-[3px] transition-all duration-300 bg-base-content/90'></div>
+                                <div className="absolute left-[50%] group-hover:left-0 w-0 group-hover:w-full h-[3px] transition-all duration-300 bg-base-content/90"></div>
                             </a>
                         </li>
                     );
                 })}
             </ul>
 
-            <div className='md:w-1/5 flex items-center justify-end gap-2'>
+            <div className="md:w-1/5 flex items-center justify-end gap-2">
                 <ThemeToggle />
                 <LangSwitch />
-                <details className='flex md:hidden dropdown dropdown-end'>
-                    <summary className='btn btn-ghost p-0'>
+                <details className="flex md:hidden dropdown dropdown-end">
+                    <summary className="btn btn-ghost p-0">
                         <MdMenu size={18} />
                     </summary>
-                    <ul className='menu dropdown-content z-[100] p-2 shadow bg-base-100 opacity-100 rounded-box w-52'>
+                    <ul className="menu dropdown-content z-[100] p-2 shadow bg-base-100 opacity-100 rounded-box w-52">
                         {linkList.map((link, index) => {
                             return (
                                 <li key={index}>
