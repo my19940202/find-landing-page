36718d7057109085c1c383b6ac81a705a87a6b15
xishengbo <xishengbo@baidu.com>
2024-10-13 18:09:19 +0800

    add google analytics

--- components/common/head.js
+++ components/common/head.js
@@ -3,20 +3,20 @@ import React from "react";
 export default function CustomHead() {
     return (
         <>
-            {/* <script
+            <script
                 async
-                src='https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXX'
+                src='https://www.googletagmanager.com/gtag/js?id=G-X8HTQKWCMY'
             ></script>
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-XXXXXXXX');
-        `,
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-X8HTQKWCMY');
+                    `,
                 }}
-            /> */}
+            />
             <script
                 dangerouslySetInnerHTML={{
                     __html: `
